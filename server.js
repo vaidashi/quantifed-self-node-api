2e3f7562c54dcd9729b5412eb4bae294a64ddb93
Ashish Vaid <vaid50@yahoo.com>
2017-12-12 16:03:06 -0700

    endpoint for all foods created

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const foodsController = require('./lib/controllers/food')
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'QS'
@@ -11,9 +12,14 @@ app.locals.title = 'QS'
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+//////// endpoints below /////////
 app.get('/', function(request, response) {
   response.send(app.locals.title)
 })
+
+app.get('/api/v1/foods', foodsController.getFoods)
+
 
 
 if(!module.parent) {
@@ -22,4 +28,6 @@ if(!module.parent) {
   })
 }
 
+
+/////exporting/////
 module.exports = app
