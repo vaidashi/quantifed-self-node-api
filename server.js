95c8cbee45343add311d058f1f4d221112f1c547
Ashish Vaid <vaid50@yahoo.com>
2017-12-12 12:56:14 -0700

    server running

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'QS'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function(request, response) {
+  response.send(app.locals.title)
+})
+
+
+if(!module.parent) {
+  app.listen(app.get('port'), function() {
+    console.log(app.locals.title + " is running on " + app.get('port') + ".")
+  })
+}
+
+module.exports = app
